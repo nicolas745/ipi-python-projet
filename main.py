07781b920dcfae761bc0fb3562b95de9f519af04
nicolas <47307856+nicolas745@users.noreply.github.com>
2024-03-12 14:59:05 +0100

    modication du code en class et add class sql pour sqlite

--- main.py
+++ main.py
@@ -1,21 +1,28 @@
 #!.venv/bin/python3
 from flask import Flask, render_template
-app = Flask(__name__)
+import sqlite3
+
+class sql():
+    def __init__(self,namefile) -> None:
+        self.conn = sqlite3.connect(namefile)
 
 
-def render(**args):
-    return render_template("index.html",**args)
-
-@app.get("/")
-def index():
-    varible = {
-        "page":"index.html"
-    }
-    return render(**varible)
-if __name__ == '__main__':
-    args = {
-        'port':8080, #comme ameloration je peut utlise os.getenv('port') mais il faut charger le fichier lib python-dotenv la meme chose pour les autre
-        'debug':True,
-        'host':"0.0.0.0"
-    }
-    app.run(**args)
+class web():
+    def __init__(self) -> None:
+        app = Flask(__name__)
+        @app.get("/")
+        def index():
+            varible = {
+                "page":"index.html"
+            }
+            return self.render(**varible)
+        if __name__ == '__main__':
+            args = {
+                'port':8080, #comme ameloration je peut utlise os.getenv('port') mais il faut charger le fichier lib python-dotenv la meme chose pour les autre
+                'debug':True,
+                'host':"0.0.0.0"
+            }
+            app.run(**args)
+    def render(self,**args):
+        return render_template("index.html",**args)
+web()
