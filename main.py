68b4ffaf9fe27db5e889a64202c279d011b54ff9
nicolas <47307856+nicolas745@users.noreply.github.com>
2024-03-13 15:34:45 +0100

    ajou de autoloder et des page html et les gestion sql de connection

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from flask import Flask, render_template
 from hashlib import sha256
 import sqlite3
 from enum import Enum
+import inspect
 
 class env(Enum):
     USER="pseudo"
@@ -11,29 +12,42 @@ class env(Enum):
     ERROR404 ="404.html"
 class sql():
     USER="pseudo"
-
     def __init__(self,namefile:str) -> None:
         self.conn = sqlite3.connect(namefile)
         cusor =self.conn.cursor()
-        cusor.execute('''
+        cusor.execute(f'''
             CREATE TABLE IF NOT EXISTS user (
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
-                {} TEXT,
-                {} TEXT
+                {env.PASSWD.value} TEXT,
+                {env.USER.value} TEXT
             );
-        '''.format(env.PASSWD.value,env.USER.value))
+        ''')
         cusor.close()
         self.conn.commit()
-    def ischeckuser(self):
+    def ischeckuser(self,user,passwd):
         cursor=self.conn.cursor()
-        cursor.execute("SELECT * FROM user WHERE user",())
+        res=cursor.execute(f"""
+                SELECT * 
+                FROM user 
+                WHERE {env.USER.value}=? AND {env.PASSWD.value}=?
+            """,(user,passwd))
         cursor.close()
+        return res.rowcount!=0
+    def adduser(self, user, passwd):
+        cursor = self.conn.cursor()
+        cursor.execute(f"""
+            INSERT INTO user ({env.USER.value,}, {env.PASSWD.value}) 
+            VALUES (?, ?)
+        """, (user, passwd))
+        self.conn.commit()
+        cursor.close()
+
 class web():
     def __init__(self,app:Flask) -> None:
         pass
     def render(self,**args):
         return render_template("index.html",**args)
-class principal(web):
+class UnauthPageHandler(web):
     def __init__(self,app:Flask) -> None:
         self.sql = sql("database.db")
         @app.get("/")
@@ -62,9 +76,16 @@ class principal(web):
                 "page":env.ERROR404.value
             }
             return self.render(**varible), 404
-app = Flask(__name__)
-principal(app)
+
+#UnauthPageHandler(app)
 if __name__ == '__main__':
+    app = Flask(__name__)
+    global_data = globals().copy()
+    for key, value in global_data.items():
+        if inspect.isclass(value):
+            if(issubclass(value,web)):
+                value(app)
+
     args = {
         'port':8080,  #comme ameloration je peut utlise os.getenv('port') mais il faut charger le fichier lib python-dotenv la meme chose pour les autre
         'debug':True,
